95ec82010fb0e6c9a88a0ad1b9c71f9cf749eae8
PavanPK07 <kalyanreddypallala@gmail.com>
2025-04-22 21:25:02 +0530

    jenkins-pipeline

---
+++ 05-app-alb/variables.tf
@@ -0,0 +1,22 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {}
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "pkpj.store"
+}
